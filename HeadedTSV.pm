9acbeedf8d4fee9c02b1f5849e21668ae224db4e
Stuart Bennett <stuart@lodestar.eng.cam.ac.uk>
2012-04-16 15:26:39 +0100

    Abstract headed tab separated variable format from TG.pm

---
+++ HeadedTSV.pm
@@ -0,0 +1,90 @@
+package HeadedTSV;
+
+use strict;
+use warnings;
+
+our $VERSION = '1.00';
+
+use base 'Exporter';
+
+our @EXPORT = qw(read_htsv write_htsv);
+
+sub read_htsv
+{
+	my ($file, $hdg_key) = @_;
+	$hdg_key = 'Headings' unless defined $hdg_key;
+	my %content;
+	my $in_header = 1;
+
+	open(FH, "<$file") or die;
+	while (<FH>) {
+		chomp;			# no newline
+		s/#.*//;		# no comments
+		s/^[ \r\n\v\f]+//;	# no leading white, except tab
+		s/\s+$//;		# no trailing white
+		next unless length;	# anything left?
+		($_) = (/^(.*)$/);	# forcably untaint file input.  if it's bad it shouldn't have got there.
+
+		$in_header = 0 if m/^===/;
+
+		if ($in_header) {
+			my ($key, $value) = split(' ', $_, 2);
+			$content{$key} = $value;
+		} else {
+			next if m/^===/;
+			unless ($content{$hdg_key}) {
+				$content{$hdg_key} = [ split ];
+			} else {
+				my @line = split('	', $_, scalar(@{$content{$hdg_key}}));
+				foreach my $i (0 .. $#{$content{$hdg_key}}) {
+					push(@{$content{$content{$hdg_key}[$i]}}, $line[$i]) if exists $line[$i];
+				}
+			}
+		}
+	}
+	close(FH);
+
+	return %content;
+}
+
+sub write_htsv
+{
+	my ($file, $content, $given_ts, $hdg_key, $max_rows) = @_;
+	my $ts = $given_ts ? $given_ts : 8;
+	$hdg_key = 'Headings' unless defined $hdg_key;
+	my $heading_only = 1;
+
+	open(FH, ">$file") or die;
+	foreach my $key (keys $content) {
+		say FH ((exists $content->{$key}) ? "$key	$content->{$key}" : "$key") unless ref($content->{$key});
+		$heading_only = 0 if ref($content->{$key});
+	}
+
+	unless ($heading_only) {
+		$max_rows = scalar @{$content->{$content->{$hdg_key}[0]}} unless defined $max_rows;
+		my $col_line = '=' x ($ts - 1) . '	';
+		my $cols = scalar(@{$content->{$hdg_key}});
+
+		print FH $col_line foreach (2 .. $cols);
+		say FH '==================================================';
+
+		foreach my $key (@{$content->{$hdg_key}}) {
+			print FH "$key	";
+		}
+		print FH "\n";
+
+		print FH $col_line foreach (2 .. $cols);
+		say FH '==================================================';
+
+		foreach my $row (0 .. ($max_rows - 1)) {
+			foreach my $key (@{$content->{$hdg_key}}) {
+				print FH "$content->{$key}[$row]	";
+			}
+			print FH "\n";
+		}
+	}
+
+	say FH "\n# vim: ts=$ts" unless ($heading_only and not defined $given_ts);;
+
+	close(FH);
+}
--- TG.pm
+++ TG.pm
@@ -2,6 +2,7 @@ package TG;
 
 use strict;
 use warnings;
+use HeadedTSV;
 
 our $VERSION = '1.00';
 
@@ -11,37 +12,7 @@ our @EXPORT = qw(read_tg write_tg);
 
 sub read_tg
 {
-	my $file = $_[0];
-	my %content;
-	my $in_header = 1;
-
-	open(FH, "<$file") or die;
-	while (<FH>) {
-		chomp;			# no newline
-		s/#.*//;		# no comments
-		s/^[ \r\n\v\f]+//;	# no leading white, except tab
-		s/\s+$//;		# no trailing white
-		next unless length;	# anything left?
-		($_) = (/^(.*)$/);	# forcably untaint file input.  if it's bad it shouldn't have got there.
-
-		$in_header = 0 if m/^===/;
-
-		if ($in_header) {
-			my ($key, $value) = split(' ', $_, 2);
-			$content{$key} = $value;
-		} else {
-			next if m/^===/;
-			unless ($content{Headings}) {
-				$content{Headings} = [ split ];
-			} else {
-				my @line = split('	', $_, scalar(@{$content{Headings}}));
-				foreach my $i (0 .. $#{$content{Headings}}) {
-					push(@{$content{$content{Headings}[$i]}}, $line[$i]) if exists $line[$i];
-				}
-			}
-		}
-	}
-	close(FH);
+	my %content = read_htsv($_[0]);
 
 	($content{Headings}[0] eq 'Creditor') or die "tg data is broken ($content{Headings}[0])";
 	($content{Headings}[1] eq 'Amount') or die "tg data is broken ($content{Headings}[1])";
@@ -58,32 +29,5 @@ sub write_tg
 	($content{Headings}[1] eq 'Amount') or die "tg data is broken ($content{Headings}[1])";
 	($content{Headings}[$#{$content{Headings}}] eq 'Description') or die "tg data is broken ($content{Headings}[$#{$content{Headings}}])";
 
-	open(FH, ">$file") or die;
-	foreach my $key (keys %content) {
-		say FH ((exists $content{$key}) ? "$key	$content{$key}" : "$key") unless ref($content{$key});
-	}
-
-	my $cols = scalar(@{$content{Headings}});
-
-	print FH '==========	' foreach (2 .. $cols);
-	say FH '==================================================';
-
-	foreach my $key (@{$content{Headings}}) {
-		print FH "$key	";
-	}
-	print FH "\n";
-
-	print FH '==========	' foreach (2 .. $cols);
-	say FH '==================================================';
-
-	foreach my $row (0 .. $#{$content{Creditor}}) {
-		foreach my $key (@{$content{Headings}}) {
-			print FH "$content{$key}[$row]	";
-		}
-		print FH "\n";
-	}
-
-	say FH "\n# vim: ts=11";
-
-	close(FH);
+	write_htsv($file, \%content, 11);
 }
