35902dc6403a0922a4784e407147fb768b2da521
Stuart Bennett <stuart@lodestar.eng.cam.ac.uk>
2012-06-16 14:52:28 +0100

    HeadedTSV.pm: PerlCritic fixes
    1) all functions must return
    2) three arg open
    3) no globally scoped filehandles

--- HeadedTSV.pm
+++ HeadedTSV.pm
@@ -17,22 +17,24 @@ my $write_encoder;
 sub set_htsv_encoders
 {
 	($read_encoder, $write_encoder) = @_;
+	return;
 }
 
 sub read_htsv
 {
 	my ($file, $nexist_ok, $hdg_key) = @_;
 	$hdg_key = 'Headings' unless defined $hdg_key;
+	my $fh;
 	my %content;
 	my $in_header = 1;
 
 	if ($nexist_ok) {
 		no autodie qw(open);
-		open(FH, "<$file") or return %content;
+		open ($fh, '<', $file) or return %content;
 	} else {
-		open(FH, "<$file") or confess "$file: $!";
+		open ($fh, '<', $file) or confess "$file: $!";
 	}
-	while (<FH>) {
+	while (<$fh>) {
 		chomp;			# no newline
 		s/^#.*//;		# no leading comments
 		s/\s#.*//;		# no whitespace prefixed comments
@@ -44,21 +46,21 @@ sub read_htsv
 		$in_header = 0 if m/^===/;
 
 		if ($in_header) {
-			my ($key, $value) = split(' ', $_, 2);
+			my ($key, $value) = split (' ', $_, 2);
 			$content{$key} = $value;
 		} else {
 			next if m/^===/;
 			unless ($content{$hdg_key}) {
 				$content{$hdg_key} = [ split ];
 			} else {
-				my @line = split('	', $_, scalar(@{$content{$hdg_key}}));
+				my @line = split ('	', $_, scalar(@{$content{$hdg_key}}));
 				foreach my $i (0 .. $#{$content{$hdg_key}}) {
 					push (@{$content{$content{$hdg_key}[$i]}}, $line[$i]);
 				}
 			}
 		}
 	}
-	close(FH);
+	close $fh;
 
 	$read_encoder->(\%content) if ($read_encoder);
 
@@ -74,11 +76,11 @@ sub write_htsv
 
 	$write_encoder->($content) if ($write_encoder);
 
-	open (FH, ">$file.new") or confess "$file.new: $!";
+	open (my $fh, '>', "$file.new") or confess "$file.new: $!";
 	foreach my $key (keys $content) {
 		unless (ref ($content->{$key})) {
 			# check if non-white exists (since trailing white killed on read anyway)
-			say FH ((defined $content->{$key} and $content->{$key} =~ /\S/) ? "$key	$content->{$key}" : "$key");
+			say $fh ((defined $content->{$key} and $content->{$key} =~ /\S/) ? "$key	$content->{$key}" : "$key");
 		} else {
 			$heading_only = 0;
 		}
@@ -89,30 +91,32 @@ sub write_htsv
 		my $col_line = '=' x ($ts - 1) . '	';
 		my $cols = scalar(@{$content->{$hdg_key}});
 
-		print FH $col_line foreach (2 .. $cols);
-		say FH '==================================================';
+		print $fh $col_line foreach (2 .. $cols);
+		say $fh '==================================================';
 
 		foreach my $key (@{$content->{$hdg_key}}) {
-			print FH "$key	";
+			print $fh "$key	";
 		}
-		print FH "\n";
+		print $fh "\n";
 
-		print FH $col_line foreach (2 .. $cols);
-		say FH '==================================================';
+		print $fh $col_line foreach (2 .. $cols);
+		say $fh '==================================================';
 
 		foreach my $row (0 .. ($max_rows - 1)) {
 			foreach my $key (@{$content->{$hdg_key}}) {
-				print FH $content->{$key}[$row] ? "$content->{$key}[$row]	" : '	';
+				print $fh $content->{$key}[$row] ? "$content->{$key}[$row]	" : '	';
 			}
-			print FH "\n";
+			print $fh "\n";
 		}
 	}
 
-	say FH "\n# vim: ts=$ts" unless ($heading_only and not defined $given_ts);;
+	say $fh "\n# vim: ts=$ts" unless ($heading_only and not defined $given_ts);
 
-	close (FH);
+	close $fh;
 
 	rename ("$file.new", $file) or confess "$file: $!";
+
+	return;
 }
 
 1;
