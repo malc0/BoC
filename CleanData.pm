2b8976629d96234f9b289694503361c1ebd9a158
Stuart Bennett <stuart@lodestar.eng.cam.ac.uk>
2012-09-16 00:53:12 +0100

    CleanData.pm: more picky clean_decimal test

--- CleanData.pm
+++ CleanData.pm
@@ -67,6 +67,7 @@ sub clean_decimal
 {
 	return 0 unless defined $_[0];
 	return 0 if ($_[0] =~ /^\s*$/);
+	return undef if ($_[0] =~ /^\s*-?[.,·]?\s*$/);
 	return undef unless $_[0] =~ /^\s*(-?\d*[.,·]?\d*)\s*$/;
 	my $num_str = $1;
 	$num_str =~ tr/,·/../;
