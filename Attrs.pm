5b80b8b904d162e1b37b5f29564715440b163bb9
Stuart Bennett <stuart@lodestar.eng.cam.ac.uk>
2012-09-25 13:19:22 +0100

    Attrs.pm: attr group recursion!

--- Attrs.pm
+++ Attrs.pm
@@ -25,23 +25,41 @@ sub get_attrs
 	return sort keys %{{read_htsv($cfg_file, 1)}};
 }
 
+sub expand_syns
+{
+	my ($syns, $want, $stop) = @_;
+
+	$stop->{$want} = 1;
+	return ($want, map ((exists $syns->{$_}) ? expand_syns($syns, $_, $stop) : $_, grep (!(exists $stop->{$_}), @{$syns->{$want}})));
+}
+
+sub uniq {
+	return keys %{{ map { $_ => 1 } @_ }};
+}
+
 sub get_attr_synonyms
 {
 	my %attrs = read_htsv($cfg_file, 1);
-	my %syns;
+	my %implied;
+	my %syns1;
+	my %synsx;
 
 	foreach my $syn (keys %attrs) {
-		push (@{$syns{$syn}}, $syn);
 		next unless $attrs{$syn};
 
 		(my $implieds = $attrs{$syn}) =~ s/\s*//g;
 		
-		foreach my $imp (split (':', $implieds)) {
-			push (@{$syns{$imp}}, $syn) unless grep ($_ eq $imp, @{$syns{$imp}});
-		}
+		push (@{$syns1{$_}}, $syn) foreach (split (':', $implieds));
 	}
 
-	return %syns;
+	foreach my $syn (keys %syns1) {
+		@{$synsx{$syn}} = map (expand_syns(\%syns1, $_, { $syn => 1 }), @{$syns1{$syn}});
+	}
+	push (@{$synsx{$_}}, $_) foreach (keys %attrs);
+
+	@{$synsx{$_}} = uniq(@{$synsx{$_}}) foreach (keys %synsx);
+
+	return %synsx;
 }
 
 1;
