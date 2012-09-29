f6e1fb726ce6def7c8e84fc28a5ab81ab7c21713
Stuart Bennett <stuart@lodestar.eng.cam.ac.uk>
2012-09-29 14:14:59 +0100

    boc.pl: much more efficient to do the array as a string

--- boc.pl
+++ boc.pl
@@ -266,7 +266,7 @@ sub read_htsv_encode
 
 	foreach my $key (keys %$content) {
 		$content->{$key} = encode_for_html($content->{$key}) unless (ref($content->{$key}) || !$content->{$key});
-		@{$content->{$key}} = map (encode_for_html($_), @{$content->{$key}}) if ref ($content->{$key});
+		@{$content->{$key}} = split ('&#9;', encode_for_html(join ('	', map ((defined) ? $_ : '', @{$content->{$key}})))) if ref ($content->{$key});
 		$content->{encode_for_html($key)} = delete $content->{$key};
 	}
 
